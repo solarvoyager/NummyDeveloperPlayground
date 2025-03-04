53510888a32d52180464af3a555f2619b6591d4c
Hamid Musayev <hemidvsmusayev@gmail.com>
2025-03-04 23:46:46 +0400

    package versions 1.4.0

--- PlaygroundApplication/Controllers/ApplicationController.cs
+++ PlaygroundApplication/Controllers/ApplicationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Nummy.CodeLogger.Data.Entitites;
 using Nummy.CodeLogger.Data.Services;
 
 namespace PlaygroundApplication.Controllers;
@@ -17,8 +18,8 @@ public class ApplicationController : ControllerBase
     [HttpGet("Test")]
     public async Task<IActionResult> Get()
     {
-        //await _loggerService.LogInfoAsync("This is info title", "Description of me");
-        //await _loggerService.LogAsync(NummyCodeLogLevel.Fatal, new ArgumentNullException("asda"));
+        await _loggerService.LogInfoAsync("This is info title", "Description of me");
+        await _loggerService.LogAsync(NummyCodeLogLevel.Fatal, new ArgumentNullException("asda"));
         //await _loggerService.LogErrorAsync(new ArgumentNullException("asda"));
 
         throw new ArgumentOutOfRangeException("rreses");
