5023d079616b9e96b1af3cd8a801c2944a7fa416
Hamid Musayev <68116432+HamidMusayev@users.noreply.github.com>
2025-11-29 15:51:32 +0400

    readme refactor

--- PlaygroundApplication/Program.cs
+++ PlaygroundApplication/Program.cs
@@ -2,6 +2,8 @@ using System.Dynamic;
 using System.Net;
 using Nummy.CodeLogger.Extensions;
 using Nummy.ExceptionHandler.Extensions;
+using Nummy.HealthChecker.Entites;
+using Nummy.HealthChecker.Extensions;
 using Nummy.HttpLogger.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,16 +12,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// From Nummy
 const string nummyServiceUrl = "http://localhost:8082/";
-
 const string applicationId = "909e2c2f-48d3-47d2-9971-c96334c2f12c";
 
+// Nummy.CodeLogger config
 builder.Services.AddNummyCodeLogger(options => 
 {
     options.NummyServiceUrl = nummyServiceUrl;
     options.ApplicationId = applicationId;
 });
 
+// Nummy.ExceptionHandler config
 dynamic errorResponse = new ExpandoObject();
 errorResponse.success = false;
 errorResponse.message = "error catched & logged by nummy exception handler";
@@ -33,6 +37,7 @@ builder.Services.AddNummyExceptionHandler(options =>
     options.NummyServiceUrl = nummyServiceUrl;
 });
 
+// Nummy.HttpLogger config
 builder.Services.AddNummyHttpLogger(options =>
 {
     options.EnableRequestLogging = true;
@@ -43,10 +48,31 @@ builder.Services.AddNummyHttpLogger(options =>
     options.NummyServiceUrl = nummyServiceUrl;
 });
 
+// Nummy.HealthChecker config
+builder.Services.AddNummyHealthChecker(options =>
+{
+    options.Path = "nummy/health";
+
+    options.CheckAsync = async (sp, ct) =>
+    {
+        // Example: use services from DI
+        // var db = sp.GetRequiredService<MyDbContext>();
+        // await db.Database.ExecuteSqlRawAsync("SELECT 1", ct);
+
+        // For now: simple “OK”
+        return new NummyHealthResult
+        {
+            IsHealthy = true,
+            Message = "Service is healthy"
+        };
+    };
+});
+
 var app = builder.Build();
 
 app.UseNummyHttpLogger();
 app.UseNummyExceptionHandler();
+app.MapNummyHealthChecker();
 
 app.UseSwagger();
 app.UseSwaggerUI();
